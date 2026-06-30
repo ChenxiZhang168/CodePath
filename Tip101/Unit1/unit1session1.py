2c7ab471928e956066ebfed18a8ff49d4324b7cc
cx_zhang_legion_GitHub_Desktop <89880234+ChenxiZhang168@users.noreply.github.com>
2026-06-30 16:23:44 -0400

    Initial commit

---
+++ Tip101/Unit1/unit1session1.py
@@ -0,0 +1,27 @@
+# Unit 1: Session 1
+
+# Problem 1: Hello World!
+# Given the following lines of code, work with your group members to place the lines in order and write and call your first Python function!
+# a. print("Hello world!")
+# b. def hello_world():
+# c. hello_world()
+
+# The correct order of the lines of code is:
+def hello_world():
+    print("Hello world!")
+hello_world()
+
+# Problem 2: Today's Mood
+# The following function uses a variable, mood to print out "Today's mood: 😎". Copy this code into your IDE and update the mood variable to print out your mood for today.
+# def todays_mood():
+#     mood = "😎"
+#     print("Today's mood: " + mood)
+# todays_mood()
+# Example Output: Today's mood: 🥱
+def todays_mood():
+    mood = "🥱"
+    print("Today's mood: " + mood)
+
+todays_mood()
+
+
